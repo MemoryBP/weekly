b1e6d19e0e08a54034215ff334685155a5670875
memorybp <809558630@qq.com>
2017-07-12 14:52:56 +0800

    默认

--- src/main/java/com/example/demo/until/BaseCronTrigger.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.until;
-
-import java.io.Serializable;
-
-/**
- * Created by 陈 on 2017/7/10.
- */
-public class BaseCronTrigger extends CronTriggerBean implements Serializable {
-}
